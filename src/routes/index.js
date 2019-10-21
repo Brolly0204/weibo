fe0d3c190957ff637cf6ffd4e7e503db3d98146c
L.W.L <102495553@qq.com>
2019-10-21 20:01:33 +0800

    feat: 路由演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,17 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const { username } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    username
   }
 })
 
